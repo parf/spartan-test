ffcf06dceec8c9353bd94fcf8cc067d4d8533077
Sergey Porfiriev <parf@comfi.com>
2013-04-09 16:03:50 -0400

    write ** testing before test

--- examples/init.php
+++ examples/init.php
@@ -1,4 +1,4 @@
-<?
+<?php
 // by deafult spartan test looks for init.php in script directory and it's parent directories
 // by default spartan test allows notices in init.php file
 // you can't have warnings in init.php
