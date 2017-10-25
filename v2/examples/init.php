dd152c0fcac2414180ff9162d74158da70cc30b0
Sergey Porfiriev <parf@comfi.com>
2017-10-25 12:37:45 -0400

    FEAT: junk removed

--- v2/examples/init.php
+++
@@ -1,5 +0,0 @@
-<?php
-// by deafult spartan test looks for init.php in script directory and it's parent directories
-// by default spartan test allows notices in init.php file
-// you can't have warnings in init.php
-?>
